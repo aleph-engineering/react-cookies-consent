ceee857e62c71f905ad1c5af310dd2276345738d
partner <partner@aleph.engineering>
2018-10-10 12:31:11 -0400

    Implement logic for saving cookie in the browser

--- src/index.js
+++ src/index.js
@@ -1,27 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { COOKIES_CONSENT_DEFAULT_MESSAGE, COOKIES_CONSENT_BUTTON_MESSAGE } from './constants';
+import Cookies from 'js-cookie';
+import {
+    COOKIES_CONSENT_BUTTON_MESSAGE,
+    COOKIES_CONSENT_DEFAULT_MESSAGE,
+    COOKIES_ID,
+} from './constants';
 import './styles.css';
 
-const CookiesConsent = ({ message, buttonMessage }) => (
-    <div className="cookies-consent">
-        <span className="cookies-consent-text">{message}</span>
-        <span>
-            <button type="button" className="cookies-consent-button">
-                {buttonMessage}
-            </button>
-        </span>
-    </div>
-);
+export default class CookiesConsent extends Component {
+    state = {
+        cookiesConsent: false,
+    };
+
+    componentWillMount() {
+        const { cookieId } = this.props;
+        const cookiesConsent = Cookies.get(cookieId);
+        if (cookiesConsent === 'true') {
+            this.enableCookiesConsent();
+        }
+    }
+
+    enableCookiesConsent = () => {
+        this.setState(prevState => ({
+            ...prevState,
+            cookiesConsent: true,
+        }));
+    };
+
+    handleConsentAccepted = () => {
+        const { cookieId } = this.props;
+        Cookies.set(cookieId, 'true');
+        this.enableCookiesConsent();
+    };
+
+    render() {
+        const { message, buttonMessage } = this.props;
+        const { cookiesConsent } = this.state;
+        return (
+            <div>
+                {cookiesConsent === false && (
+                    <div className="cookies-consent">
+                        <span className="cookies-consent-text">{message}</span>
+                        <span>
+                            <button
+                                type="button"
+                                className="cookies-consent-button"
+                                onClick={this.handleConsentAccepted}>
+                                {buttonMessage}
+                            </button>
+                        </span>
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
 
 CookiesConsent.propTypes = {
+    cookieId: PropTypes.string,
     message: PropTypes.string,
     buttonMessage: PropTypes.string,
 };
 
 CookiesConsent.defaultProps = {
+    cookieId: COOKIES_ID,
     message: COOKIES_CONSENT_DEFAULT_MESSAGE,
     buttonMessage: COOKIES_CONSENT_BUTTON_MESSAGE,
 };
-
-export default CookiesConsent;
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { COOKIES_CONSENT_DEFAULT_MESSAGE, COOKIES_CONSENT_BUTTON_MESSAGE } from './constants';
+import {
+    COOKIES_CONSENT_DEFAULT_MESSAGE,
+    COOKIES_CONSENT_BUTTON_MESSAGE,
+    COOKIES_ID,
+} from './constants';
 import CookiesConsent from '.';
 
 describe('Suite of tests for the CookiesConsent component', () => {
@@ -11,34 +15,47 @@ describe('Suite of tests for the CookiesConsent component', () => {
 
         expect(wrapper.find('span.cookies-consent-text')).toBeDefined();
         expect(wrapper.find('span.cookies-consent-text')).toHaveLength(1);
+        expect(wrapper.props().message).toBe(COOKIES_CONSENT_DEFAULT_MESSAGE);
         expect(wrapper.find('span.cookies-consent-text').get(0).props.children).toBe(
             COOKIES_CONSENT_DEFAULT_MESSAGE
         );
 
         expect(wrapper.find('button.cookies-consent-button')).toBeDefined();
         expect(wrapper.find('button.cookies-consent-button')).toHaveLength(1);
+        expect(wrapper.props().buttonMessage).toBe(COOKIES_CONSENT_BUTTON_MESSAGE);
         expect(wrapper.find('button.cookies-consent-button').get(0).props.children).toBe(
             COOKIES_CONSENT_BUTTON_MESSAGE
         );
+
+        expect(wrapper.props().cookieId).toBe(COOKIES_ID);
     });
 
     test('CookiesConsent component should render messages passed as a prop', () => {
+        const cookieIdText = 'my-unique-id';
         const cookiesConsentText = 'We use cookies';
         const cookiesConsentButtonText = 'Accept';
         const wrapper = mount(
-            <CookiesConsent message={cookiesConsentText} buttonMessage={cookiesConsentButtonText} />
+            <CookiesConsent
+                message={cookiesConsentText}
+                buttonMessage={cookiesConsentButtonText}
+                cookieId={cookieIdText}
+            />
         );
 
         expect(wrapper.find('span.cookies-consent-text')).toBeDefined();
         expect(wrapper.find('span.cookies-consent-text')).toHaveLength(1);
+        expect(wrapper.props().message).toBe(cookiesConsentText);
         expect(wrapper.find('span.cookies-consent-text').get(0).props.children).toBe(
             cookiesConsentText
         );
 
         expect(wrapper.find('button.cookies-consent-button')).toBeDefined();
         expect(wrapper.find('button.cookies-consent-button')).toHaveLength(1);
+        expect(wrapper.props().buttonMessage).toBe(cookiesConsentButtonText);
         expect(wrapper.find('button.cookies-consent-button').get(0).props.children).toBe(
             cookiesConsentButtonText
         );
+
+        expect(wrapper.props().cookieId).toBe(cookieIdText);
     });
 });
